e9d98194fef32e428dc127e9758ad0e95c7989b9
Ryota Ikezawa <pavegy@gmail.com>
2022-10-08 20:38:42 +0900

    :art: Update routes namespace

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import { Profile, Root } from './containers';
 
-export const Router = createBrowserRouter([
+export const AppRoutes = createBrowserRouter([
   { path: '/', element: <Root /> },
   { path: '/profile', element: <Profile /> }
 ]);
