f71b690a4fb5b064a4723289037d1003b6fc479a
Ryota Ikezawa <pavegy@gmail.com>
2022-10-08 20:32:34 +0900

    re added husky and lint-stagged

---
+++ src/containers/index.tsx
@@ -0,0 +1,5 @@
+import { Profile } from './Profile';
+import { Root } from './Root';
+
+// Exporting all containers
+export { Profile, Root };
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import { Profile } from './containers/Profile';
-import { Root } from './containers/Root';
+import { Profile, Root } from './containers';
 
 export const Router = createBrowserRouter([
   { path: '/', element: <Root /> },
