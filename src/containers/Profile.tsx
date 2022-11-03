16e3b49e2e6dd0a663cc7bb83506bb61913e0264
Ryota Ikezawa <pavegy@gmail.com>
2022-11-03 21:30:15 +0900

    :art: Update careers

--- src/containers/Profile.tsx
+++ src/containers/Profile.tsx
@@ -1,9 +1,11 @@
 import { Container, Title, Group, Spoiler, List, Text } from '@mantine/core';
 import React, { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { ProfileCard } from '../components/ProfileCard';
 import { Layout } from '../Layout';
 
 export const Profile: FC = () => {
+  const { t } = useTranslation();
   return (
     <>
       <Layout>
@@ -12,17 +14,30 @@ export const Profile: FC = () => {
             About Me
           </Title>
           <Title mb='lg' order={2} size='h3'>
-            Careers
+            {t('careerTitle')}
           </Title>
           <Group mb='xl'>
             <Spoiler hideLabel='Hide' maxHeight={120} showLabel='Show more'>
               <List>
                 <List.Item>
                   <Title order={3} size='h5' underline>
-                    Network Engineer@
-                    <Text component='span'>XTOP technology.inc</Text>{' '}
-                    <Text component='span'>(Apr 2015 - Oct 2015)</Text>
+                    Software Engineer@
+                    <Text
+                      component='a'
+                      href='https://www.freee.co.jp/'
+                      target='_blank'
+                      variant='link'
+                    >
+                      Freee K.K.
+                    </Text>{' '}
+                    <Text component='span'>(Nov 2015 - Present)</Text>
                   </Title>
+                  <List listStyleType='disc' withPadding>
+                    <List.Item>{t('freeeDetail1')}</List.Item>
+                    <List.Item>{t('freeeDetail2')}</List.Item>
+                    <List.Item>{t('freeeDetail3')}</List.Item>
+                    <List.Item>{t('freeeDetail4')}</List.Item>
+                  </List>
                 </List.Item>
                 <List.Item>
                   <Title order={3} size='h5' underline>
@@ -37,20 +52,21 @@ export const Profile: FC = () => {
                     </Text>{' '}
                     <Text component='span'>(Aug 2018 - Aug 2019)</Text>
                   </Title>
+                  <List listStyleType='disc' withPadding>
+                    <List.Item>{t('acallDetail1')}</List.Item>
+                    <List.Item>{t('acallDetail2')}</List.Item>
+                    <List.Item>{t('acallDetail3')}</List.Item>
+                  </List>
                 </List.Item>
                 <List.Item>
                   <Title order={3} size='h5' underline>
-                    Software Engineer@
-                    <Text
-                      component='a'
-                      href='https://www.freee.co.jp/'
-                      target='_blank'
-                      variant='link'
-                    >
-                      Freee K.K.
-                    </Text>{' '}
-                    <Text component='span'>(Nov 2015 - Present)</Text>
+                    Network Engineer@
+                    <Text component='span'>XTOP technology.inc</Text>{' '}
+                    <Text component='span'>(Apr 2015 - Oct 2015)</Text>
                   </Title>
+                  <List listStyleType='disc' withPadding>
+                    <List.Item>{t('xtopDetail1')}</List.Item>
+                  </List>
                 </List.Item>
               </List>
             </Spoiler>
