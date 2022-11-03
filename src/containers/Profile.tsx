e0d3d160a315bfffab770af31082f75a73c9eba4
Ryota Ikezawa <pavegy@gmail.com>
2022-11-03 21:04:16 +0900

    :recycle: Improved careers

--- src/containers/Profile.tsx
+++ src/containers/Profile.tsx
@@ -17,30 +17,41 @@ export const Profile: FC = () => {
           <Group mb='xl'>
             <Spoiler hideLabel='Hide' maxHeight={120} showLabel='Show more'>
               <List>
-                <Title order={3} size='h5' underline>
-                  <Text component='span'>XTOP technology.inc</Text>{' '}
-                  <Text component='span'>(Apr 2015 - Oct 2015)</Text>
-                </Title>
-                <List.Item>Network Engineer</List.Item>
-                <Title order={3} size='h5' underline>
-                  <Text component='a' href='https://corp.acall.jp/' target='_blank' variant='link'>
-                    ACALL K.K.
-                  </Text>{' '}
-                  <Text component='span'>(Aug 2018 - Aug 2019)</Text>
-                </Title>
-                <List.Item>Application Engineer as a side job</List.Item>
-                <Title order={3} size='h5' underline>
-                  <Text
-                    component='a'
-                    href='https://www.freee.co.jp/'
-                    target='_blank'
-                    variant='link'
-                  >
-                    Freee K.K.
-                  </Text>{' '}
-                  <Text component='span'>(Nov 2015 - Present)</Text>
-                </Title>
-                <List.Item>Software Engineer</List.Item>
+                <List.Item>
+                  <Title order={3} size='h5' underline>
+                    Network Engineer@
+                    <Text component='span'>XTOP technology.inc</Text>{' '}
+                    <Text component='span'>(Apr 2015 - Oct 2015)</Text>
+                  </Title>
+                </List.Item>
+                <List.Item>
+                  <Title order={3} size='h5' underline>
+                    Application Engineer@
+                    <Text
+                      component='a'
+                      href='https://corp.acall.jp/'
+                      target='_blank'
+                      variant='link'
+                    >
+                      ACALL K.K.
+                    </Text>{' '}
+                    <Text component='span'>(Aug 2018 - Aug 2019)</Text>
+                  </Title>
+                </List.Item>
+                <List.Item>
+                  <Title order={3} size='h5' underline>
+                    Software Engineer@
+                    <Text
+                      component='a'
+                      href='https://www.freee.co.jp/'
+                      target='_blank'
+                      variant='link'
+                    >
+                      Freee K.K.
+                    </Text>{' '}
+                    <Text component='span'>(Nov 2015 - Present)</Text>
+                  </Title>
+                </List.Item>
               </List>
             </Spoiler>
           </Group>
